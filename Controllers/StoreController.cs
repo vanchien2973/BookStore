40506c5027e23ece63665d4324cc6af8d4630880
ducminh1392003neverup <130551819+ducminh1392003neverup@users.noreply.github.com>
2023-12-04 20:55:51 +0700

    Create StoreController.cs Create Homepage(User)

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Store
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Book.Include(b => b.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Store/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        private bool BookExists(int id)
+        {
+          return (_context.Book?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
